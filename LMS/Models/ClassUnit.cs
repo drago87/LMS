93aa9a1ac08199ac7a13b621c50ca202aac4dbb7
drago87 <dr.drago.87@hotmail.com>
2016-12-20 10:05:33 +0100

    Added more models

--- LMS/Models/ClassUnit.cs
+++ LMS/Models/ClassUnit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 
@@ -10,5 +11,8 @@ namespace LMS.Models
     {
         [Key]
         public int ClassUnitID { get; set; }
+        public string ClassName { get; set; }
+        [ForeignKey("Folder")]
+        public Folder Folders { get; set; }
     }
 }
